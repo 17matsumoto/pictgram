1ca624a7f7529137e46ce04f3ac19905bf0aa5de
17matsumoto <17yuya.baseball@gmail.com>
2023-06-10 16:12:39 +0900

    トップ画面の作成

---
+++ src/main/java/com/example/pictgram/controller/PagesController.java
@@ -0,0 +1,16 @@
+package com.example.pictgram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PagesController {
+	
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("通ってる？");
+		return "pages/index";
+	}
+
+}
